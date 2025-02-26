4cb783d438c7b1d8a77363c4bacd607bacb8e2a5
Saurav Chaudhary <chaudharygroup2015@gmail.com>
2025-02-26 19:04:56 +0530

    Deploying on Render.com

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,6 +1,4 @@
-const JwtValidation = require("../middlewares/JwtValidation");
 const router = require("express").Router();
-const { applyJob } = require("../controllers/UserController");
 
 // Test route
 router.get("/test", (req, res) => {
